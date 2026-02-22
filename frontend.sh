f527fcc25ada3ae7b0cb33a158a40048dc9ece00
kavya Poreddy <poreddykavya1207@gmail.com>
2026-02-22 16:01:48 +0530

    sessions

---
+++ frontend.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+MONGODB_HOST=mongodb.daws88s.sbs
+SCRIPT_DIR=$(pwd)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privilege"
+    exit 1 # failure is other than 0
+fi
+
+VALIDATE(){ #functions receive inputs through args just like shell script args
+   if [ $1 -ne 0 ]; then
+   echo -e " $2 ... $R FAILURE $N" | tee -a $LOG_FILE
+   exit 1
+   else
+    echo -e " $2 ... $G SUCCESS $N" | tee -a $LOG_FILE
+   fi 
+}
+
+dnf module disable nginx -y &>>$LOG_FILE
+dnf module enable nginx:1.24 -y &>>$LOG_FILE
+dnf install nginx -y &>>$LOG_FILE
+VALIDATE $? "installing nginx"
+
+systemctl enable nginx &>>$LOG_FILE
+systemctl start nginx 
+VALIDATE $? "starting nginx"
+
+rm -rf /usr/share/nginx/html/* 
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
+cd /usr/share/nginx/html 
+unzip /tmp/frontend.zip &>>$LOG_FILE
+VALIDATE $? "dowloading front end"
+
+rm -rf /etc/nginx/nginx.conf
+cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
+VALIDATE $? "copying nginx"
+
+systemctl restart nginx 
+VALIDATE $? "restarting nginx"
